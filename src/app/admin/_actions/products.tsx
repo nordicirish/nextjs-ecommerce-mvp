829ad18d6b1e6c57fe3371790062a4e274a5f12e
Roger Graham <rogergraham147@gmail.com>
2024-08-28 17:42:15 +0300

    Implement addProduct action and ProductForm

---
+++ src/app/admin/_actions/products.tsx
@@ -0,0 +1,59 @@
+"use server";
+import { z } from "zod";
+import db from "@/db/db";
+import fs from "fs/promises";
+import { redirect } from "next/navigation";
+
+const fileSchema = z.instanceof(File, { message: "File is required" });
+
+// file size === 0, if no file is provided don't validate the image type
+const imageSchema = fileSchema.refine(
+  (file) => file.size === 0 || file.type.startsWith("image/")
+);
+
+// schema is more specific file and image schemas so they can be reused for editing
+const addSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+  // form returns a string so coerce to number
+  priceInCents: z.coerce.number().int().min(1),
+  file: fileSchema.refine((file) => file.size > 0, "File is required"),
+  image: imageSchema.refine((file) => file.size > 0, "Image is required"),
+});
+export async function addProduct(prevState: unknown, formData: FormData) {
+  const result = addSchema.safeParse(Object.fromEntries(formData.entries()));
+  console.log(result);
+  // return errors for each field that fails validation to the user
+  if (!result.success) {
+    return result.error.formErrors.fieldErrors;
+  }
+  const data = result.data;
+  await fs.mkdir("products", { recursive: true });
+  //create a unique file name to acoid overwrite conflicts
+  const filePath = `products/${crypto.randomUUID()} - ${data.file.name}`;
+
+  // write the file in a format that can be read by node
+  await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+
+  await fs.mkdir("public/products", { recursive: true });
+  //create a unique image name to avoid overwrite conflicts
+  const imagePath = `/products/${crypto.randomUUID()} - ${data.image.name}`;
+
+  // write the image in a format that can be read by node
+  // image path already starts with a /
+  await fs.writeFile(
+    `public${imagePath}`,
+    Buffer.from(await data.image.arrayBuffer())
+  );
+  db.product.create({
+    data: {
+      isAvailableForPurchase: false,
+      name: data.name,
+      description: data.description,
+      priceInCents: data.priceInCents,
+      filePath,
+      imagePath,
+    },
+  });
+  redirect("/admin/products");
+}
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -7,16 +7,19 @@ import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
 import { addProduct } from "../../_actions/products";
+import { useFormState, useFormStatus } from "react-dom";
 
 export function ProductForm() {
+  const [error, action]   = useFormState(addProduct, {});
   const [priceInCents, setPriceInCents] = useState<number>();
   console.log(priceInCents);
   return (
-    <form action={addProduct}className="space-y-8">
+    <form action={action}className="space-y-8">
       <div className="space-y-2">
         {/* htmlFor is used to associate the label with the input */}
         <Label htmlFor="name">Name</Label>
         <Input type="text" id="name" name="name" required />
+        {error?.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
         {/* htmlFor is used to associate the label with the input */}
@@ -29,6 +32,7 @@ export function ProductForm() {
           value={priceInCents}
           onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         />
+        {error?.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
       </div>
       <div className="text-muted-foreground">
         {formatCurrency((priceInCents || 0) / 100)}
@@ -36,17 +40,28 @@ export function ProductForm() {
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
         <Textarea id="description" name="description" required />
+        {error?.description && <div className="text-destructive">{error.description}</div>}
       </div>
       <div className="space-y-2">
         {/* htmlFor is used to associate the label with the input */}
         <Label htmlFor="file">File</Label>
         <Input type="file" id="file" name="file" required />
+        {error?.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
       <Label htmlFor="image">Image</Label>
         <Input type="file" id="image" name="image" required />
+        {error?.image && <div className="text-destructive">{error.image}</div>}
       </div>
-      <Button type="submit">Save</Button>
+      <SubmitButton/>
     </form>
   );
 }
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button type="submit" disabled={pending}> {pending ? "Saving..." : "Save"}</Button>
+  )
+}
+
