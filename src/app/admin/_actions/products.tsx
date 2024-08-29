bab4a85c4204fe93d03a9a6130bc3cfc602f8c57
Roger Graham <rogergraham147@gmail.com>
2024-08-29 15:55:47 +0300

    Add deleteProduct and toggleProductAvailability server actions

--- src/app/admin/_actions/products.tsx
+++ src/app/admin/_actions/products.tsx
@@ -3,6 +3,7 @@ import { z } from "zod";
 import db from "@/db/db";
 import fs from "fs/promises";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 const fileSchema = z.instanceof(File, { message: "File is required" });
 
@@ -45,15 +46,45 @@ export async function addProduct(prevState: unknown, formData: FormData) {
     `public${imagePath}`,
     Buffer.from(await data.image.arrayBuffer())
   );
-  db.product.create({
+  try {
+    await db.product.create({
+      data: {
+        isAvailableForPurchase: false,
+        name: data.name,
+        description: data.description,
+        priceInCents: data.priceInCents,
+        filePath,
+        imagePath,
+      },
+    });
+  } catch (error) {
+    console.error("Db error creating product:", error);
+  }
+  revalidatePath("/");
+  revalidatePath("/products");
+  redirect("/admin/products");
+}
+
+export async function deleteProduct(id: string) {
+  try {
+    const product = await db.product.delete({ where: { id } });
+    if (product === null) {
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    console.error("Db error deleting product:", error);
+  }
+}
+export async function toggleProductAvailability(
+  id: string,
+  isAvailableForPurchase: boolean
+) {
+  await db.product.update({
+    where: {
+      id,
+    },
     data: {
-      isAvailableForPurchase: false,
-      name: data.name,
-      description: data.description,
-      priceInCents: data.priceInCents,
-      filePath,
-      imagePath,
+      isAvailableForPurchase,
     },
   });
-  redirect("/admin/products");
 }
