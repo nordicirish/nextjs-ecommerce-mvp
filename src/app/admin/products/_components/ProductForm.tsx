248174ca0bea39d4ed3796a16e17cfe6a3c096c8
Roger Graham <rogergraham147@gmail.com>
2024-08-30 15:54:01 +0300

    Import updateProduct Action and modify form to accept updateProduct props if product exists

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,19 +6,32 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { formatCurrency } from "@/lib/formatters";
 import { useState } from "react";
-import { addProduct } from "../../_actions/products";
+import { addProduct, updateProduct } from "../../_actions/products";
 import { useFormState, useFormStatus } from "react-dom";
-
-export function ProductForm() {
-  const [error, action]   = useFormState(addProduct, {});
-  const [priceInCents, setPriceInCents] = useState<number>();
+import { Product } from "@prisma/client";
+import Image from "next/image";
+// Change action depending on if product is null or not to addProduct or updateProduct
+export function ProductForm({ product }: { product?: Product | null }) {
+  const [error, action] = useFormState(product == null ? addProduct : updateProduct.bind(null, product.id), {});
+  const [
+    priceInCents,
+    //for editing return priceInCents from product
+    setPriceInCents,
+  ] = useState<number | undefined>(product?.priceInCents);
   console.log(priceInCents);
   return (
-    <form action={action}className="space-y-8">
+    <form action={action} className="space-y-8">
       <div className="space-y-2">
         {/* htmlFor is used to associate the label with the input */}
         <Label htmlFor="name">Name</Label>
-        <Input type="text" id="name" name="name" required />
+        {/* for editing return name from product */}
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          required
+          defaultValue={product?.name || ""}
+        />
         {error?.name && <div className="text-destructive">{error.name}</div>}
       </div>
       <div className="space-y-2">
@@ -32,28 +45,61 @@ export function ProductForm() {
           value={priceInCents}
           onChange={(e) => setPriceInCents(Number(e.target.value) || undefined)}
         />
-        {error?.priceInCents && <div className="text-destructive">{error.priceInCents}</div>}
+        {error?.priceInCents && (
+          <div className="text-destructive">{error.priceInCents}</div>
+        )}
       </div>
       <div className="text-muted-foreground">
         {formatCurrency((priceInCents || 0) / 100)}
       </div>
       <div className="space-y-2">
         <Label htmlFor="description">Description</Label>
-        <Textarea id="description" name="description" required />
-        {error?.description && <div className="text-destructive">{error.description}</div>}
+        <Textarea
+          id="description"
+          name="description"
+          required
+          defaultValue={product?.description || ""}
+        />
+        {error?.description && (
+          <div className="text-destructive">{error.description}</div>
+        )}
       </div>
       <div className="space-y-2">
         {/* htmlFor is used to associate the label with the input */}
         <Label htmlFor="file">File</Label>
-        <Input type="file" id="file" name="file" required />
+        <Input
+          type="file"
+          id="file"
+          name="file"
+          // only required if adding a new product
+          required={product === null}
+        />
+        {/* for editing return filePath from product */}
+        {product != null && (
+          <div className="text-muted-foreground">{product.filePath}</div>
+        )}
         {error?.file && <div className="text-destructive">{error.file}</div>}
       </div>
       <div className="space-y-2">
-      <Label htmlFor="image">Image</Label>
-        <Input type="file" id="image" name="image" required />
+        <Label htmlFor="image">Image</Label>
+        <Input
+          type="file"
+          id="image"
+          name="image"
+          // only required if adding a new product
+          required={product === null}
+        />
+        {product != null && (
+          <Image
+            src={product.imagePath}
+            width={200}
+            height={200}
+            alt={`${product.name} image`}
+          />
+        )}
         {error?.image && <div className="text-destructive">{error.image}</div>}
       </div>
-      <SubmitButton/>
+      <SubmitButton />
     </form>
   );
 }
@@ -61,7 +107,9 @@ export function ProductForm() {
 function SubmitButton() {
   const { pending } = useFormStatus();
   return (
-    <Button type="submit" disabled={pending}> {pending ? "Saving..." : "Save"}</Button>
-  )
+    <Button type="submit" disabled={pending}>
+      {" "}
+      {pending ? "Saving..." : "Save"}
+    </Button>
+  );
 }
-
