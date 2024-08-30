648f0d14f6be13bc7fcefa7131a1a68738781d2d
Roger Graham <rogergraham147@gmail.com>
2024-08-30 15:57:28 +0300

    Add upDateProduct action

--- src/app/admin/_actions/products.tsx
+++ src/app/admin/_actions/products.tsx
@@ -1,8 +1,9 @@
 "use server";
+// server actions
 import { z } from "zod";
 import db from "@/db/db";
 import fs from "fs/promises";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 
 const fileSchema = z.instanceof(File, { message: "File is required" });
@@ -64,13 +65,71 @@ export async function addProduct(prevState: unknown, formData: FormData) {
   revalidatePath("/products");
   redirect("/admin/products");
 }
+const editSchema = addSchema.extend({
+  // file and image are optional so they can be left blank when editing
+  file: fileSchema.optional(),
+  image: imageSchema.optional(),
+});
+export async function updateProduct(
+  id: string,
+  prevState: unknown,
+  formData: FormData
+) {
+  const result = editSchema.safeParse(Object.fromEntries(formData.entries()));
+  console.log(result);
+  // return errors for each field that fails validation to the user
+  if (!result.success) {
+    return result.error.formErrors.fieldErrors;
+  }
+  const data = result.data;
+  const product = await db.product.findUnique({ where: { id } });
+  if (product === null) return notFound();
+  let filePath = product.filePath;
+  // delete the old file if a new one is uploaded
+  if (data.file != null && data.file.size > 0) {
+    await fs.unlink(product.filePath);
+    filePath = `products/${crypto.randomUUID()} - ${data.file.name}`;
+
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()));
+  }
+  let imagePath = product.imagePath;
+  // delete the old image if a new one is uploaded
+  if (data.image != null && data.image.size > 0) {
+    await fs.unlink(`public${product.imagePath}`);
+    imagePath = `/products/${crypto.randomUUID()} - ${data.image.name}`;
+    await fs.writeFile(
+      `public${imagePath}`,
+      Buffer.from(await data.image.arrayBuffer())
+    );
+  }
+  try {
+    await db.product.update({
+      where: {
+        id,
+      },
+      data: {
+        name: data.name,
+        description: data.description,
+        priceInCents: data.priceInCents,
+        filePath,
+        imagePath,
+      },
+    });
+  } catch (error) {
+    console.error("Db error creating product:", error);
+  }
+  revalidatePath("/");
+  revalidatePath("/products");
+  redirect("/admin/products");
+}
 
 export async function deleteProduct(id: string) {
   try {
     const product = await db.product.delete({ where: { id } });
-    if (product === null) {
-      throw new Error("Product not found");
-    }
+    if (product === null) return notFound();
+    //unlink deletes the file and image from server storage
+    await fs.unlink(product.filePath);
+    await fs.unlink(`public${product.imagePath}`);
   } catch (error) {
     console.error("Db error deleting product:", error);
   }
